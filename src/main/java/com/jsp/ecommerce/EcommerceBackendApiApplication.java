29d7a4f6372925c2e99b355a275da349162dd93e
Gouse Raza <gouse.raza20@gmail.com>
2025-12-31 23:01:48 +0530

    Initial Spring Boot e-commerce backend setup

---
+++ src/main/java/com/jsp/ecommerce/EcommerceBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.jsp.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jsp/ecommerce/enums/UserRole.java
@@ -0,0 +1,6 @@
+package com.jsp.ecommerce.enums;
+
+public enum UserRole {
+	ADMIN, USER, MERCHANT
+
+}
