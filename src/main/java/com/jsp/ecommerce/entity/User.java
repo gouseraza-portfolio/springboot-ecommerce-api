b2dc40cf995d8f1a0cb679f4267a4dbbdf24a007
Gouse Raza <gouse.raza20@gmail.com>
2026-01-05 23:22:51 +0530

    Implemented JWT authentication and role based authorization

--- src/main/java/com/jsp/ecommerce/entity/User.java
+++ src/main/java/com/jsp/ecommerce/entity/User.java
@@ -9,10 +9,14 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
